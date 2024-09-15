0a13aefedd246a394be433c5269c5d2fac95341c
Cheryl2312 <dsouzacheryl44@gmail.com>
2024-09-15 21:28:59 +0100

    changes set 8

---
+++ app.js
@@ -0,0 +1,27 @@
+// List of local cake images
+const cakeImages = [
+    'anniversary.jpg',
+    'birthday.jpg',
+    'graduation.jpg',
+    'babyshower.jpg',
+    'cupcakes.jpg',
+    'fondant.jpg',
+    'graduation.jpg',
+    'layer.jpg',
+    'wedding.jpg'
+];
+
+function getRandomCakeImage() {
+// Generate a random index
+const randomIndex = Math.floor(Math.random() * cakeImages.length);
+// Get the image URL
+const imageUrl = cakeImages[randomIndex];
+// Set the image src
+document.getElementById('custom-cake-image').src = imageUrl;
+}
+
+// Change the cake image every 2 seconds
+setInterval(getRandomCakeImage, 2000);
+
+// Load a random cake image on page load
+window.onload = getRandomCakeImage;
